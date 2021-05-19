14948181396ea78e0f8b1efcb655b3a4d8624055
Maxi25554 <maxin25554@gmail.com>
2021-05-19 13:29:57 +1000

    Fixes

--- PTPScript.py
+++ PTPScript.py
@@ -8,27 +8,25 @@ import random
 def random_color():
     col = random.choice(['green', 'red', 'blue'])
     return (col)
+#    return ('black')
 
 
-def lmat(mtag, depth):
-    abc = mtag
+def lmat(mtag):
     file = open("mat/"+mtag.strip()+".txt")
     content = file.readlines()
     mats = int(content[1])
     graph.add_node(pydot.Node(mtag, shape='circle', fillcolor = 'lightblue', style = 'filled', fontname="helvetica"))
     colour = random_color()
-    if mats > 0:
-        for i in range(mats):
-            x = (i*2+2)
-            y = content[x]
-            new_edge = pydot.Edge(mtag, y, color= colour, label = content[x+1], dir="back", minlen = 2, )
-            graph.add_edge(new_edge)
-            lmat(y, (depth+1))
+    for i in range(mats):
+        x = (i*2+2)
+        y = content[x]
+        new_edge = pydot.Edge(mtag, y, color= colour, label = content[x+1], dir="back", minlen = 2, )
+        graph.add_edge(new_edge)
+        lmat(y)
 
-#graph = pydot.Dot('ctree_graph', graph_type='graph', bgcolor='white', suppress_disconnected = False, simplify = True)
 
 def graphing(tags):
-    lmat (tags, 1)
+    lmat (tags)
     graph.write_raw('ctreedot.dot')
     graph.set_size("80,80!")
     graph.write_png('output.png')
@@ -38,4 +36,4 @@ suppress_disconnected = False, simplify = True, concentrate = True, splines = 'p
 
 ##Edit the variable below
 
-graphing("HYPERSHIP")
+graphing("CQL")
