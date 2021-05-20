cf6acf54f4d853bec2185f868e542c8aa1cfb537
Maxi25554 <maxin25554@gmail.com>
2021-05-20 10:50:37 +1000

    Itemlist

--- PTPScript.py
+++ PTPScript.py
@@ -1,5 +1,6 @@
 import pydot
 import random
+import os
 #import networkx as nx
 #import matplotlib.pyplot as plt
 #import matplotlib
@@ -9,7 +10,7 @@ def random_color():
     col = random.choice(['green', 'red', 'blue'])
     return (col)
 #    return ('black')
-
+mdict = {}
 
 def lmat(mtag):
     file = open("mat/"+mtag.strip()+".txt")
@@ -17,24 +18,41 @@ def lmat(mtag):
     mats = int(content[1])
     graph.add_node(pydot.Node(mtag, shape='circle', fillcolor = 'lightblue', style = 'filled', fontname="helvetica"))
     colour = random_color()
+    if mtag in mdict:
+        mdict[mtag] += 1
+    else:
+        mdict[mtag] = 1
     for i in range(mats):
         x = (i*2+2)
         y = content[x]
         new_edge = pydot.Edge(mtag, y, color= colour, label = content[x+1], dir='back', minlen = 2, )
         graph.add_edge(new_edge)
+        num = float(content[x+1])
+        if y in mdict:
+            mdict[y] += num
+        else:
+            mdict[y] = (num-1)
         lmat(y)
 
 
 def graphing(tags):
+    open('matlist.txt', 'w').close()
     lmat (tags)
-#    graph.write_raw('ctreedot.dot')
     graph.set_size("80,80!")
-#    graph.write_pdf('output.pdf')
     graph.write_png('output.png')
+    print (mdict)
+    for m, b in mdict.items():
+        mlist = str(m.strip() + " = " + str(b))
+        print (mlist)
+        f = open("matlist.txt", "a")
+        f.write(mlist + "\n")
+        f.close()
+
+
 
 graph = pydot.Dot('ctree_graph', graph_type='graph', bgcolor = 'white', \
 suppress_disconnected = False, simplify = True, concentrate = True, splines = 'polyline')
 
 ##Edit the variable below
 
-graphing("MINSHIP")
+graphing("HOS")
