682f8485d1814afa0dff000b619c75f7b6087ac5
Maxi25554 <maxin25554@gmail.com>
2021-05-21 17:51:01 +1000

    Major optimisation - render time for hypership cut from 40 seconds to 3

--- PTPScript.py
+++ PTPScript.py
@@ -11,10 +11,11 @@ itemcode = "HYPERSHIP"
 #Whether the connecting lines should be coloured
 coloured = True
 #Whether a zip file of the output should be created
-zip = True
+zip = False
 
 #End of variables. Don't touch anything below this point unless you know what you're doing.
-
+nodelist = []
+edgelist = []
 
 mdict = {}
 mnat = open(game.strip()+"nats.txt")
@@ -30,19 +31,25 @@ else:
             col = 'black'
             return (col)
 
+graph = pydot.Dot('ctree_graph', graph_type='graph', bgcolor = 'white', \
+suppress_disconnected = False, simplify = True, concentrate = True, splines = 'polyline')
 
 def lmat(mtag, man):
     file = open(game.strip()+"mat/"+mtag.strip()+".txt")
     content = file.readlines()
     mats = int(content[1])
-    graph.add_node(pydot.Node(mtag, shape='circle', fillcolor = 'lightblue', style = 'filled', fontname="helvetica"))
+    if mtag not in nodelist:
+        graph.add_node(pydot.Node(mtag, shape='circle', fillcolor = 'lightblue', style = 'filled', fontname="helvetica"))
+        nodelist.append(mtag)
     colour = random_color()
     for i in range(mats):
         x = (i*2+2)
         y = content[x]
         co = float(content[x+1])
-        new_edge = pydot.Edge(mtag, y, color = colour, label = round(co, 2), dir ='back', minlen = 2, decorate = True)
-        graph.add_edge(new_edge)
+        if (mtag + y) not in edgelist:
+            new_edge = pydot.Edge(mtag, y, color = colour, label = round(co, 2), dir ='back', minlen = 2, decorate = True)
+            graph.add_edge(new_edge)
+            edgelist.append(mtag+y)
         num = co*man
         if y in mdict:
             mdict[y] += num
@@ -70,8 +77,7 @@ def graphing(tags):
         shutil.make_archive("zips/"+tags.strip()+"zip", 'zip', "output")
 
 
-graph = pydot.Dot('ctree_graph', graph_type='graph', bgcolor = 'white', \
-suppress_disconnected = False, simplify = True, concentrate = True, splines = 'polyline')
+
 
 
 graphing(itemcode)
